87b0d4bc1dc440394850b13aa0fa4fe9aa49c954
edgarsantillan <edgarale_27@hotmail.com>
2020-08-07 00:11:59 -0500

    first commit on github

---
+++ Dtos/UserForLoginDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.API.Dtos
+{
+    public class UserForLoginDto
+    {
+        [Required]
+        [EmailAddress(ErrorMessage = "Enter a valid email address.")]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; } 
+    }
+}
